908547a0ddbdb892243b3ff0fe062b391c1ec1b5
Connor Claxton <czclaxton@gmail.com>
2022-02-20 14:35:39 -0500

    final result with readme

--- index.js
+++ index.js
@@ -1,3 +1,69 @@
-process.argv.forEach((val, index) => {
-  console.log(`${index}: ${val}`);
-});
+const fs = require("fs");
+const readline = require("readline");
+
+// const data = fs.readFileSync("data.txt", "utf-8");
+
+Array.prototype.getLastDuplicate = function (obj) {
+  for (let i = 0; i < this.length; i++) {
+    if (this[i].score === obj.score) return { obj, i };
+  }
+  return null;
+};
+
+(async function parseFileLines() {
+  try {
+    let output = [];
+    const inputFile = process.argv[3] || "data.txt";
+
+    if (process.argv[3] === undefined) process.argv[3] = "data.txt";
+
+    console.log(`Input File: ${inputFile}`);
+
+    const rl = readline.createInterface({
+      input: fs.createReadStream(inputFile),
+      crlfDelay: Infinity,
+    });
+
+    rl.on("line", (line) => {
+      const isolatedJSON = line.substring(
+        line.indexOf("{") - 1,
+        line.lastIndexOf("}") + 1
+      );
+
+      if (!isolatedJSON) {
+        console.log("THIS IS NOT JSON");
+        process.exit(9);
+      }
+
+      const formattedObj = {
+        score: parseInt(line.match(/\d+/g)[0]),
+        id: JSON.parse(isolatedJSON).id,
+      };
+
+      const checkForDupes = output.getLastDuplicate(formattedObj);
+
+      if (checkForDupes === null) {
+        output.push(formattedObj);
+      } else {
+        output[checkForDupes.i] = checkForDupes.obj;
+      }
+    });
+
+    // output = output.filter((entry) => {
+    //   if (output.some((entry) => entry.score === "b")) {
+    //     entry[entry.score] = true;
+    //     return true;
+    //   }
+    //   return false;
+    // }, Object.create(null));
+
+    rl.on("close", () => {
+      output.sort((a, b) => {
+        return b.score - a.score;
+      });
+      console.log("FINAL OUTPUT: ", output.slice(0, process.argv[2]));
+    });
+  } catch (err) {
+    console.error(err);
+  }
+})();
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Instructions
+
+Different txt files have been created to cater towards triggering different failure cases. If no file is specified data.txt is used by default.
+
+```
+node index.js n file
+```
+
+### Example script execution:
+
+```
+node index.js 10 repeatingScore.txt
+```
